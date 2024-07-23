8131cb6f76d48b751187540f56944a55414cc2f2
Garrett <me@garrettfoy.com>
2024-07-22 20:12:03 -0500

    initial shadcn + tailwind + sveltekit skeleton project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button class="text-md px-5">
+    Hello World!
+</Button>
